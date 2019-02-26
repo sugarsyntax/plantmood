adfe43401af973fea84456ff5406cb8936d37f6a
Jacquelline Beltran <jacquelline500@gmail.com>
2019-02-25 19:21:30 -0800

    Add info pages and color

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/info'
   root 'plants#index'
   resources :plants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
